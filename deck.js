1ecec30e346138a699602f988cae70ce987b21d2
Mastervoliumpl <kubafijalkowski25@gmail.com>
2024-09-13 00:06:18 +0200

    Seperated all classes into seperate files

---
+++ deck.js
@@ -0,0 +1,42 @@
+class Deck {
+    constructor(cardStylePath) {
+        this.cardStylePath = cardStylePath;
+        this.cards = this.createDeck();
+        this.shuffleDeck();
+    }
+
+    createDeck() {
+        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
+        const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+        let deck = [];
+        for (let suit of suits) {
+            for (let value of values) {
+                let numericValue = this.getNumericValue(value);
+                const imagePath = `${this.cardStylePath}${suit[0]}${value[0]}.jpg`;
+                deck.push(new Card(suit, value, numericValue, imagePath));
+            }
+        }
+        return deck;
+    }
+
+    getNumericValue(value) {
+        console.log('Getting Numeric Value');
+        if (value === 'Ace') return 11;
+        if (value === 'Jack' || value === 'Queen' || value === 'King') return 10;
+        console.log('Numeric Value:', parseInt(value));
+        return parseInt(value);
+    }
+
+    // Fisher–Yates shuffle
+    shuffleDeck() {
+        console.log('shuffling deck');
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+        }
+    }
+
+    drawCard() {
+        return this.cards.pop();
+    }
+}
---
+++ player.js
@@ -0,0 +1,28 @@
+class Player {
+    constructor() {
+        this.hand = [];
+        this.score = 0;
+        this.bust = false;
+    }
+
+    addCard(card) {
+        this.hand.push(card);
+        this.calculateScore();
+    }
+
+    calculateScore() {
+        let aces = 0;
+        console.log('calculating score');
+        this.score = this.hand.reduce((acc, card) => { // reduce method to sum the numeric value of each card in the hand, acc is the accumulator (Keeps accumulating the sum of the cards), card is the current card being iterated
+            if (card.value === 'Ace') aces++;
+            return acc + card.numericValue; // returns the sum of the accumulator and the numeric value of the current card
+        }, 0); // 0 is the initial value of the accumulator
+
+        while (this.score > 21 && aces > 0) {
+            this.score -= 10;
+            aces--;
+        }
+
+        this.bust = this.score > 21;
+    }
+}
